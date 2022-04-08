401ad21162ddee61fa5dee0e5d390955e7dff141
Eddy20015 <edwardj5@hotmail.com>
2022-04-07 18:39:55 -0700

    Fixed The Lobby
    Going to see if I can try password protecting tonight!

--- Elevator Game Project/Assets/Scripts/Photon Scripts/PlayerItem.cs
+++ Elevator Game Project/Assets/Scripts/Photon Scripts/PlayerItem.cs
@@ -8,9 +8,6 @@ using UnityEngine.UI;
 public class PlayerItem : MonoBehaviourPunCallbacks
 {
     [SerializeField] private TMPro.TMP_Text PlayerName;
-    [SerializeField] private GameObject HatButton;
-    [SerializeField] private GameObject HatPanel;
-    private bool HatPanelUp = false;
 
     //this is stuff for the hat selector stuff
     ExitGames.Client.Photon.Hashtable PlayerProperties = new ExitGames.Client.Photon.Hashtable();   //photon's version of a hashtable
@@ -24,24 +21,9 @@ public class PlayerItem : MonoBehaviourPunCallbacks
 
     public void ApplyLocalChanges()
     {
-        HatButton.SetActive(true);
+    
     }
-
-    public void OnClickHatButton()
-    {
-        if (!HatPanelUp)
-        {
-            HatPanel.SetActive(true);
-            HatPanelUp = true;
-        }
-        else
-        {
-            HatPanel.SetActive(false);
-            HatPanelUp = false;
-        }
 
         //there is a lot more info on the character selection portion on
         //the Blackthornprod Multiplater character selection tutorial
-        
-    }
 }
