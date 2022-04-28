ddd614205883d5bd3fdaf4682ed5953865cc796f
Eddy20015 <edwardj5@hotmail.com>
2022-04-28 00:37:00 -0700

    Sprint Bar Works Correctly Now
    YAAAAS

--- Elevator Game Project/Assets/Scripts/Lights/Light1.cs
+++ Elevator Game Project/Assets/Scripts/Lights/Light1.cs
@@ -1,8 +1,9 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Photon.Pun;
 
-public class Light1 : MonoBehaviour
+public class Light1 : MonoBehaviourPunCallbacks
 {
     //Tim Kashani
 
@@ -11,23 +12,45 @@ public class Light1 : MonoBehaviour
     [SerializeField] GameObject pointLight;
 
     //temporary if I think of a better solution
-    GameObject player;
+    private GameObject player;
+    private PhotonView playerView;
 
     // Start is called before the first frame update
     void Start()
     {
-        player = GameObject.FindGameObjectWithTag("Player");
+        if(GameStateManager.GetPlayState() == GameStateManager.PLAYSTATE.LOCAL)
+        {
+            player = GameObject.FindGameObjectWithTag("Player");
+        }
+        else if(GameStateManager.GetPlayState() == GameStateManager.PLAYSTATE.ONLINE)
+        {
+            GameObject[] players = GameObject.FindGameObjectsWithTag("Player");
+            if (players[0].GetPhotonView().IsMine && PhotonNetwork.IsMasterClient)
+            {
+                player = players[0];
+                playerView = player.GetPhotonView();
+            }
+            else if(players[0].GetPhotonView().IsMine && !PhotonNetwork.IsMasterClient)
+            {
+                player = players[1];
+                playerView = player.GetPhotonView();
+            }
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Vector3.Distance(transform.position, player.transform.position) > 100)
+        if (playerView != null)
         {
-            pointLight.SetActive(false);
-        } else
-        {
-            pointLight.SetActive(true);
+            if (Vector3.Distance(transform.position, player.transform.position) > 100)
+            {
+                pointLight.SetActive(false);
+            }
+            else
+            {
+                pointLight.SetActive(true);
+            }
         }
     }
 
