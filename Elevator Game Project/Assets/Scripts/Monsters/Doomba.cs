01e195ecd345869c54fff25f393f151e8ed63ab2
bhsueh1 <57551711+bhsueh1@users.noreply.github.com>
2022-05-27 17:55:31 -0700

    did some testing and edits to lobby

--- Elevator Game Project/Assets/Scripts/Monsters/Doomba.cs
+++ Elevator Game Project/Assets/Scripts/Monsters/Doomba.cs
@@ -17,6 +17,7 @@ public class Doomba : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        view = GetComponent<PhotonView>();
         agent = GetComponent<NavMeshAgent>();
         Patrol();
     }
