4b583bc015528d2fe1e095850351f56fba6f4930
tim-kashani <tim@fireapples.com>
2022-05-30 00:23:07 -0700

    added titlescreenmusic singleton script

---
+++ Elevator Game Project/Assets/Scripts/Audio/TitleScreenMusic.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TitleScreenMusic : MonoBehaviour
+{
+    public static TitleScreenMusic titleScreenMusic;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //Singleton (Only one of these scripts can exist at the same time)
+
+        if (titleScreenMusic == null)
+        {
+            titleScreenMusic = this;
+        } else
+        {
+            Destroy(gameObject);
+        }
+
+        DontDestroyOnLoad(gameObject);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public static void Destroy()
+    {
+        //Destroys music when starting a level
+
+        GameObject g = titleScreenMusic.gameObject;
+        titleScreenMusic = null;
+        Destroy(g);
+    }
+}
