262da768f546824dbacb1e113c81766888404222
Eddy20015 <edwardj5@hotmail.com>
2022-06-02 12:08:07 -0700

    Random Positions Synced

--- Elevator Game Project/Assets/Scripts/Random_Position.cs
+++ Elevator Game Project/Assets/Scripts/Random_Position.cs
@@ -1,8 +1,9 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Photon.Pun;
 
-public class Random_Position : MonoBehaviour
+public class Random_Position : MonoBehaviourPunCallbacks
 {
     public Vector3[] positions;
 
@@ -10,8 +11,21 @@ public class Random_Position : MonoBehaviour
     {
         int randomNumber = Random.Range(0, positions.Length);
 
-        transform.position = positions[randomNumber];
+        if(GameStateManager.GetPlayState() == GameStateManager.PLAYSTATE.LOCAL)
+        {
+            transform.position = positions[randomNumber];
+        }
+        else if(GameStateManager.GetPlayState() == GameStateManager.PLAYSTATE.ONLINE && PhotonNetwork.IsMasterClient)
+        {
+            transform.position = positions[randomNumber];
+            GetComponent<PhotonView>().RPC("RPC_Sync", RpcTarget.Others, positions[randomNumber]);
+        }
     }
 
+    [PunRPC]
+    private void RPC_Sync(Vector3 Placement)
+    {
+        transform.position = Placement;
+    }
     
 }
