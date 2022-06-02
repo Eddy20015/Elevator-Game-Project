eb7ca8ee7aac5ab1fa576a6ca3c1f57d273cc917
tim-kashani <tim@fireapples.com>
2022-06-02 01:59:47 -0700

    continue system works

--- Elevator Game Project/Assets/Scripts/Continue.cs
+++ Elevator Game Project/Assets/Scripts/Continue.cs
@@ -4,10 +4,12 @@ using UnityEngine;
 
 public class Continue : MonoBehaviour
 {
+    [SerializeField] int level;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        PlayerPrefs.SetInt("Level", level);
     }
 
     // Update is called once per frame
